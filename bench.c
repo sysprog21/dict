5d4372ffc065dd511137fbee1b79cd3ee7874053
siahuat0727 <tansiahuat@gmail.com>
2018-10-13 14:42:17 +0800

    Fix bug where tst_search_prefix always failed

--- bench.c
+++ bench.c
@@ -22,7 +22,7 @@ double tvgetf()
 
 int bench_test(const tst_node *root, char *out_file, const int max)
 {
-    char prefix[3] = "";
+    char prefix[4] = "";
     char word[WORDMAX] = "";
     char **sgl;
     FILE *fp = fopen(out_file, "w");
@@ -44,9 +44,9 @@ int bench_test(const tst_node *root, char *out_file, const int max)
 
     sgl = (char **) malloc(sizeof(char *) * max);
     while (fscanf(dict, "%s", word) != EOF) {
-        if (strlen(word) < 4)
+        if (strlen(word) < sizeof(prefix))
             continue;
-        strncpy(prefix, word, 3);
+        snprintf(prefix, sizeof(prefix), "%s", word);
         t1 = tvgetf();
         tst_search_prefix(root, prefix, sgl, &sidx, max);
         t2 = tvgetf();
