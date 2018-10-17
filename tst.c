308b54d0277d7f85d3060019c3eac89b6b0ce060
siahuat0727 <tansiahuat@gmail.com>
2018-10-17 08:52:40 +0800

    Fix bug where seg. fault when search prefix 'A'

--- tst.c
+++ tst.c
@@ -321,12 +321,12 @@ void tst_suggest(const tst_node *p,
                  int *n,
                  const int max)
 {
-    if (!p || *n == max)
+    if (!p || *n >= max)
         return;
     tst_suggest(p->lokid, c, nchr, a, n, max);
     if (p->key)
         tst_suggest(p->eqkid, c, nchr, a, n, max);
-    else if (*(((char *) p->eqkid) + nchr - 1) == c)
+    else if (*(((char *) p->eqkid) + nchr - 1) == c && *n < max)
         a[(*n)++] = (char *) p->eqkid;
     tst_suggest(p->hikid, c, nchr, a, n, max);
 }
